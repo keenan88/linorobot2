2097c40fec48fa9f7047f5f5c626e293bfc37a39
Keenan88 <lakingsfan08@gmail.com>
2024-10-16 15:24:56 -0400

    Chose theta star planner since it is respecting global costmap and provides meaningful orientation on path, allowing us to use path align critic in contrller trajectory generation

--- linorobot2_navigation/linorobot2_navigation/traj_visualizer.py
+++ linorobot2_navigation/linorobot2_navigation/traj_visualizer.py
@@ -19,6 +19,8 @@ class LocalPlanEvaluator(Node):
         # Publisher to RViz for visualizing trajectories
         self.marker_publisher = self.create_publisher(MarkerArray, '/local_plan_evaluation_markers', 10)
         self.get_logger().info("Subscribed to /nav2/evaluation topic")
+
+        self.id = 0
     
     def evaluation_callback(self, msg: LocalPlanEvaluation):
         self.get_logger().info(f"Received LocalPlanEvaluation: best_index = {msg.best_index}, worst_index = {msg.worst_index}")
@@ -26,8 +28,8 @@ class LocalPlanEvaluator(Node):
 
         self.get_logger().info(f"Best plan: {msg.twists[msg.best_index].traj.velocity.x, msg.twists[msg.best_index].traj.velocity.y, msg.twists[msg.best_index].traj.velocity.theta}")
         for score in msg.twists[msg.best_index].scores:
-            self.get_logger().info(f"{score.name}: {score.raw_score} * {score.scale} = {score.raw_score * score.scale}")
-        self.get_logger().info(f"{msg.twists[msg.best_index].total}")
+            self.get_logger().info(f"{score.name[0:8]}: {round(score.raw_score, 2)} * {round(100*score.scale, 1)} = {round(100*score.raw_score * score.scale, 1)}")
+        self.get_logger().info(f"                       {round(100*msg.twists[msg.best_index].total, 2)}")
         self.get_logger().info("\n")
 
         marker_array = MarkerArray()
@@ -38,7 +40,7 @@ class LocalPlanEvaluator(Node):
             marker.header.frame_id = "map"
             marker.header.stamp = self.get_clock().now().to_msg()
             marker.ns = f"trajectory_{idx}"
-            marker.id = idx
+            
             marker.type = Marker.LINE_STRIP
             marker.action = Marker.ADD
             marker.scale.x = 0.01  # Line width
@@ -46,8 +48,8 @@ class LocalPlanEvaluator(Node):
             
             # Color based on the evaluation index
             if idx == msg.best_index:
-                marker.color.r = 0.0
-                marker.color.g = 1.0
+                marker.color.r = 1.0
+                marker.color.g = 0.0
                 marker.color.b = 0.0  # Best trajectory in green
                 marker.scale.x = 0.0025
                 marker.color.a = 1.0  # Transparency
@@ -65,6 +67,9 @@ class LocalPlanEvaluator(Node):
                 marker.points.append(point)
 
             if idx == msg.best_index:
+
+                marker.id = self.id
+                #self.id += 1
         
                 marker_array.markers.append(marker)
 
